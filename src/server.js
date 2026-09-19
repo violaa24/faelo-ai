783e3c87f5ffb18b71513f7fd44f4e4589bd26a5
violaa24 <v.amelia2407@gmail.com>
2026-09-19 22:32:01 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,7 +6,8 @@ const { saveDemoRequest } = require('./repositories/demoRequestRepository');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.post('/api/demo-request', async (req, res) => {
   try {
